e9bd8044a9a0432578e56ab7f9d38191cf391857
qwasium <113295196+qwasium@users.noreply.github.com>
2024-11-01 17:37:55 +0900

    add to gitignore

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,13 +3,13 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'luk400/vim-jukit',
-    -- opts = function()
-    --   vim.g.jukit_shell_cmd = 'ipython3'
-    --   vim.g.jukit_terminal = ''
-    --   vim.g.jukit_notebook_viewer = 'code'
-    -- end,
-  },
+  -- {
+  --   'luk400/vim-jukit',
+  --   -- opts = function()
+  --   --   vim.g.jukit_shell_cmd = 'ipython3'
+  --   --   vim.g.jukit_terminal = ''
+  --   --   vim.g.jukit_notebook_viewer = 'code'
+  --   -- end,
+  -- },
 }
 -- vim: ts=2 sts=2 sw=2 et
